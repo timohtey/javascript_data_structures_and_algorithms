171a3e2df8f7bddb24db058a890363935dd47885
Timothy Rodriguez <timothy.rodriguez136@gmail.com>
2022-07-24 16:10:45 +0800

    feat(binarySearchTree): add DFS implementations

--- data_structures/binary_search_tree.js
+++ data_structures/binary_search_tree.js
@@ -72,6 +72,69 @@ class BinarySearchTree {
 
     return visited;
   }
+
+  depthFirstSearchPreOrder() {
+    const visited = [];
+
+    function traverse(node) {
+      if (node === null) {
+        return;
+      }
+
+      visited.push(node.value);
+      if (node.left !== null) {
+        traverse(node.left);
+      }
+      if (node.right !== null) {
+        traverse(node.right);
+      }
+    }
+
+    traverse(this.root);
+    return visited;
+  }
+
+  depthFirstSearchPostOrder() {
+    const visited = [];
+
+    function traverse(node) {
+      if (node === null) {
+        return;
+      }
+
+      if (node.left !== null) {
+        traverse(node.left);
+      }
+      if (node.right !== null) {
+        traverse(node.right);
+      }
+      visited.push(node.value);
+    }
+
+    traverse(this.root);
+    return visited;
+  }
+
+  depthFirstSearchInOrder() {
+    const visited = [];
+
+    function traverse(node) {
+      if (node === null) {
+        return;
+      }
+
+      if (node.left !== null) {
+        traverse(node.left);
+      }
+      visited.push(node.value);
+      if (node.right !== null) {
+        traverse(node.right);
+      }
+    }
+
+    traverse(this.root);
+    return visited;
+  }
 }
 
 let tree = new BinarySearchTree();
@@ -81,4 +144,4 @@ tree.insert(15);
 tree.insert(18);
 tree.insert(6);
 tree.insert(4);
-console.log(tree.breadthFirstSearch());
+console.log(tree.depthFirstSearchInOrder());
